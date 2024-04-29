80ca9d29b8d2c41d95a117b1f97ac0a409a6a8a2
Rolf Stargate <rolf.stargate.world@gmail.com>
2024-04-29 15:03:39 +0200

    Update

--- nvim/.config/nvim/lua/nvim/taskw.lua
+++ nvim/.config/nvim/lua/nvim/taskw.lua
@@ -1,21 +1,26 @@
 local function create_task()
-  local task = vim.fn.input('task ')
-  local current_file = vim.fn.expand('%')
   local current_line = vim.fn.line('.')
+
+  local home = vim.fn.getenv("HOME")
+  local path = vim.fn.expand('%:p')
+  local general_path = path:gsub("^" .. home, "$HOME")
 
   -- create task and annotate it with the current file and line in the format
   -- <line>:<file>
   -- this will be used by a custom action created for taskopen and defined in the 
   -- .taskopenrc file, to open the file at the line where the task was created
-  local cmd = 'task add ' .. task .. ' && task +LATEST annotate ' .. current_line .. ':' .. current_file
+  local cmd = 'task add  ' .. ' && task +LATEST annotate ' .. current_line .. ':' .. general_path
 
-  local result = vim.fn.system(cmd)
-  local success = vim.v.shell_error == 0
-
-  if success then
-    print('Task created: \n', result)
-  else
-    print('Error creating task: \n', result)
+  -- the terminal is used to have completion for tags, projects ...
+  -- 
+  vim.cmd("new | terminal")
+  local buf = vim.api.nvim_get_current_buf()
+  vim.api.nvim_input('i' .. cmd)
+  for i = 1, 500 do
+    vim.api.nvim_input('<left>')
+  end
+  for i = 1, 9 do
+    vim.api.nvim_input('<right>')
   end
 
 end
