f90e56b6d2333345638900b8f937c19f7fea6ca6
rolf <rolf.stargate.world@gmail.com>
2024-04-29 09:51:00 +0000

    Update

--- nvim/.config/nvim/lua/plugins/markdown/keymaps.lua
+++ nvim/.config/nvim/lua/plugins/markdown/keymaps.lua
@@ -125,24 +125,3 @@ wk.register({
 	mode = "n",
 	prefix = "<Leader>",
 })
-
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>oee",
-	"<cmd>TWEditTask<cr>",
-	{ desc = "TaskWarrior Edit", noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap("n", "<leader>oE", "<cmd>TWView<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>oeu", "<cmd>TWUpdateCurrent<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>oes", "<cmd>TWSyncTasks<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>oed", "<cmd>TWRunWithCurrent<cr>del<cr>dd", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<c-space>", "<cmd>TWToggle<cr>", { silent = true })
--- Be caution: it may be slow to open large files, because it scan the whole buffer
---
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
-	group = vim.api.nvim_create_augroup("TWTask", { clear = true }),
-	pattern = "*.md,*.markdown", -- Pattern to match Markdown files
-	callback = function()
-		vim.cmd("TWSyncTasks")
-	end,
-})
