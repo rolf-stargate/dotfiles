924beb54552239fbbae7ebe5ee1abffe1f3e7f04
rolf <rolf.stargate.world@gmail.com>
2024-05-14 07:05:23 +0200

    Update

--- nvim/.config/nvim/lua/filetypes/vimwiki.lua
+++ nvim/.config/nvim/lua/filetypes/vimwiki.lua
@@ -1,8 +1,9 @@
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "vimwiki",
   callback = function()
+    print("task sync")
     local cmd = "task sync"
-    local result = vim.fn.system(cmd)
+    vim.fn.system(cmd)
     local exit_code = vim.v.shell_error
 
     if exit_code ~= 0 then
