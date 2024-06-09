59697001fc22da7ea3ecc74b8d4c2f0eff722944
Rolf Stargate <rolf.stargate.world@gmail.com>
2024-06-09 12:30:10 +0200

    Update

--- nvim/.config/nvim/lua/plugins/LSP/init.lua
+++ nvim/.config/nvim/lua/plugins/LSP/init.lua
@@ -1,9 +1,2 @@
 require("plugins.LSP.cmp")
-
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-require('lspconfig').clangd.setup({})
-
+require("plugins.LSP.lsp")
---
+++ nvim/.config/nvim/lua/plugins/LSP/lsp.lua
@@ -0,0 +1,34 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('lspconfig').tsserver.setup({
+  on_init = function(client)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+})
+
+require('lspconfig').clangd.setup({
+  cmd = { "clangd", "--background-index", "--fallback-style=gnu" },
+})
+
+lsp_zero.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['tsserver'] = {'javascript', 'typescript'},
+    ['clangd'] = {'c'},
+  }
+})
+
+lsp_zero.set_sign_icons({
+  error = 'x',
+  warn = 'w',
+  hint = 'h',
+  info = 'i'
+})
+
