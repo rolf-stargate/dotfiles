3db74d095df74321f7f473521e2aae65b4eb5928
rolf <rolf.stargate.world@gmail.com>
2024-07-18 11:48:25 +0200

    Update

--- nvim/.config/nvim/lua/filetypes/vimwiki.lua
+++ nvim/.config/nvim/lua/filetypes/vimwiki.lua
@@ -1,28 +1,33 @@
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "*.md",
-  callback = function()
-    local cmd = "task sync > /dev/null 2>&1 &"
-    vim.fn.system(cmd)
-  end,
+	pattern = "*.md",
+	callback = function()
+		local cmd = "task sync > /dev/null"
+		local output = vim.fn.system(cmd)
+
+		local exit_code = vim.v.shell_error
+
+		if exit_code ~= 0 then
+			print(output)
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*.md",
-  callback = function()
-    vim.cmd("setlocal updatetime=100")
+	pattern = "*.md",
+	callback = function()
+		vim.cmd("setlocal updatetime=100")
 		vim.keymap.set(
 			"n",
 			"<leader>dd",
 			":VimwikiTable ",
 			{ buffer = true, noremap = true, desc = "Create 3x3 table" }
 		)
-  end,
+	end,
 })
 
 vim.api.nvim_create_autocmd("CursorHold", {
-  pattern = "*.md",
-  callback = function()
-    print("test")
-    vim.cmd("lua require('hover').hover()")
-  end
+	pattern = "*.md",
+	callback = function()
+		vim.cmd("lua require('hover').hover()")
+	end,
 })
