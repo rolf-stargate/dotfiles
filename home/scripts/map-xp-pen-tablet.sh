d20348b2796cedd83c450164665e84b01f6a80bc
Rolf Stargate <rolf.stargate.world@gmail.com>
2024-09-28 06:37:53 +0200

    Update

--- home/scripts/map-xp-pen-tablet.sh
+++ home/scripts/map-xp-pen-tablet.sh
@@ -3,4 +3,4 @@
 # Run from /etc/udev/rules
 
 xinput map-to-output "UGTABLET 21.5 inch PenDisplay Stylus Pen (0)" DisplayPort-0
-echo "$? --> ${date}" >> /home/rolf/foo/debug.log
+echo "$? --> $(date)" >> /home/rolf/foo/debug.log
