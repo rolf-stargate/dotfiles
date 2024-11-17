616820294c747dc8437103292cec6793323d33f2
Rolf Stargate <rolf.stargate.world@gmail.com>
2024-11-17 12:03:55 +0100

    Update

--- nvim/.config/nvim/lua/filetypes/vimwiki.lua
+++ nvim/.config/nvim/lua/filetypes/vimwiki.lua
@@ -1,3 +1,5 @@
+-- |////|__ EVENTS __|////////////////////////////////////////////////////|»)-->
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "*.md",
 	callback = function()
@@ -12,31 +14,57 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 	end,
 })
 
+vim.api.nvim_create_autocmd("CursorHold", {
+	pattern = "*.md",
+	callback = function()
+		vim.cmd("lua require('hover').hover()")
+	end,
+})
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+-- <--(«|////////////////////////////////////////////////////|__ EVENTS __|////|
+
+-- |////|__ FILETYPE __|//////////////////////////////////////////////////|»)-->
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "vimwiki",
 	callback = function()
+		-- =======  LOCAL OPTIONS  ====================================================>
 		vim.cmd("setlocal softtabstop=3")
 		vim.cmd("setlocal shiftwidth=3")
 		vim.cmd("setlocal updatetime=100")
+		-- <====================================================  LOCAL OPTIONS  =======
 
+		-- =======  MARKDOWN TABLE  ===================================================>
 		vim.keymap.set(
 			"n",
 			"<leader>dd",
 			":VimwikiTable ",
 			{ buffer = true, noremap = true, desc = "Create 3x3 table" }
 		)
+		-- <===================================================  MARKDOWN TABLE  =======
+
+		-- =======  MARKDOWN TO LATEX TO PDF  =========================================>
 		vim.keymap.set(
 			"n",
-			"<leader>lp",
+			"<leader>ol",
 			":!pandoc % -o %.pdf --template=/home/rolf/.config/nvim/templates/letter.latex && brave %.pdf<cr>",
-			{ buffer = true, noremap = true, desc = "Create 3x3 table" }
+			{ buffer = true, noremap = true, desc = "Letter" }
 		)
 		vim.keymap.set(
 			"n",
-			"<leader>lP",
-			':%s/%%.*$//g<cr>:!pandoc % --from=markdown --output=/tmp/%:t:r.pdf --variable=geometry:"margin=3cm, a4paper" && brave /tmp/%:t:r.pdf<cr><cr>u:w<cr>:nohl<cr>',
-			{ buffer = true, noremap = true, desc = "Create 3x3 table" }
+			"<leader>oa",
+			":!pandoc % -o %.pdf --template=/home/rolf/Dropbox/wiki/template_application_2024_11_08_11:08:12.latex && brave %.pdf<cr>",
+			{ buffer = true, noremap = true, desc = "Job Application" }
 		)
+		vim.keymap.set(
+			"n",
+			"<leader>op",
+			':%s/%%.*$//g<cr>:!pandoc % --from=markdown --output=/tmp/%:t:r.pdf --variable=geometry:"margin=3cm, a4paper" && brave /tmp/%:t:r.pdf<cr><cr>u:w<cr>:nohl<cr>',
+			{ buffer = true, noremap = true, desc = "Documentation" }
+		)
+		-- <=========================================  MARKDOWN TO LATEX TO PDF  =======
+
+		-- =======  LLM CHAT BUFFER  ==================================================>
 		vim.keymap.set(
 			"n",
 			"<C-g>g",
@@ -49,12 +77,8 @@ vim.api.nvim_create_autocmd("FileType", {
 			"<esc>G?rolf:<cr>:nohlsearch<cr>A ",
 			{ buffer = true, noremap = true, desc = "ChatGpt Jump to Input" }
 		)
+		-- <==================================================  LLM CHAT BUFFER  =======
 	end,
 })
-
-vim.api.nvim_create_autocmd("CursorHold", {
-	pattern = "*.md",
-	callback = function()
-		vim.cmd("lua require('hover').hover()")
-	end,
-})
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+-- <--(«|//////////////////////////////////////////////////|__ FILETYPE __|////|
