5c3b0409a8d0d3f240e28af62d07c044220e374a
Rolf Stargate <rolf.stargate.world@gmail.com>
2025-02-15 13:47:20 +0100

    Update

--- nvim/.config/nvim/lua/filetypes/vimwiki.lua
+++ nvim/.config/nvim/lua/filetypes/vimwiki.lua
@@ -125,7 +125,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.keymap.set(
 			"n",
 			"<leader>od",
-			":!md_to_pdf_live_preview % %:h/out/%:t:r.pdf<cr>",
+			":silent !md_to_pdf_live_preview % %:h/out/%:t:r.pdf<cr>",
 			{ buffer = true, noremap = true, desc = "Documentation" }
 		)
 		-- <=====================================  MARKDOWN TO LATEX TO PDF  =======
