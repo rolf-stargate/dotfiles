1d2ddd5c7a268979784ed9e4992508dd1de51622
Rolf Stargate <rolf.stargate.world@gmail.com>
2025-02-27 15:50:25 +0100

    Update

--- home/.screenlayout/5M.sh
+++ home/.screenlayout/5M.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --mode 1920x1080 --pos 0x1080 --rotate normal
+xrandr --output DP-1 --primary --mode 1920x1080 --pos 0x1611 --rotate normal --output DP-2 --mode 1920x1080 --pos 3000x1280 --rotate left --output DP-3 --mode 1280x1024 --pos 1920x0 --rotate right --output DP-4 --mode 1920x1080 --pos 1920x1280 --rotate left --output DP-5 --mode 1920x1200 --pos 0x411 --rotate normal --output DP-6 --off
